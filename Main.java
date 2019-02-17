3bb07d303f131394e70560f6b36fdec2765fafe8
Manohar Chitoda <33169063+DotOpperator@users.noreply.github.com>
2019-02-17 11:29:59 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -16,7 +16,7 @@ public class Main
 
         static String names[] = new String[55];
 
-        public static void main(String[] args) throws IOException
+        public static void main(String[] args) throws IOException, SQLException,ClassNotFoundException
         {
         Connection conn = null;
         Statement stmt = null;
@@ -28,19 +28,28 @@ public class Main
             System.out.println("Connecting to database...");
             conn = DriverManager.getConnection(DB_URL,USER,PASS);
 
-
+            /*String array[] ={"Job_Size_Max_Core_Count", "Job_Size_Min_Core_Count",
+                            "Job_Size_Per_Job_Core_Count","Number_of_Jobs_Running",
+                            "Number_of_Jobs_Started","Number_of_Jobs_Submitted",
+                            "Wait_Hours_Per_Job","Wall_Hours_Per_Job",
+                            "Wall_Hours_Total"};*/
             //STEP 4: Execute a query
             System.out.println("Creating statement...");
             stmt = conn.createStatement();
             String sql;
-            File f = new File("/Volumes/MANOHAR/Programming/DataManager/out/production/DataManager/Job_Size_Max_(Core_Count).csv");
-            Scanner sc = new Scanner(f);
+            String fileName = "Wall_Hours_Total.csv";
+            String names[] = new String[1000];
+            Scanner read = new Scanner(new FileReader(fileName));
+            names = getNames(read);
 
-            while(sc.hasNextLine())
-            {
-                String s = sc.nextLine();
-              //sql = "INSERT INTO EmployeeName(name) Values('"+ names[i++] + "');";
-                stmt.executeUpdate(sql);
+            String data[][] = new String[90][57];
+            data = getData(read);
+            for (int row = 0; row < 90; row++) {
+                for (int col = 0; col < 55; col++) {
+                    sql = "UPDATE OtherInformation SET Wall_Hours_Total = "
+                            + data[row][col + 2] + " WHERE day = " + (row+1) + " AND name = '" + names[col] + "';";
+                    stmt.executeUpdate(sql);
+                }
             }
 
             stmt.close();
@@ -72,6 +81,67 @@ public class Main
     public static long getNanoDay(int n)
     {
         return ((14984352 + n*864) * (long)Math.pow(10,11));
+    }
+
+    public static String[] getNames(Scanner read) throws IOException
+    {
+        int i = 0;
+        String str[] = new String [1000];
+        String name = "";
+        String line = read.nextLine();
+        char c = line.charAt(0);
+        int loc = 0;
+        boolean isBraket = false;
+        while(loc < line.length())
+        {
+            if(c == '[')
+            {
+                isBraket = true;
+                c = line.charAt(loc++);
+            }
+            else if (c == ']')
+            {
+                isBraket = false;
+                str[i++] = name;
+                name = "";
+            }
+            if(isBraket)
+            {
+                name = name + c;
+            }
+            c = line.charAt(loc++);
+        }
+
+        return str;
+    }
+
+    public static String[][] getData(Scanner read)
+    {
+        int loc = 0; char c = ' ';
+        String data[][] = new String[90][57];
+        int row = 0, col = 0; String field = "";
+
+        while(read.hasNextLine())
+        {
+            loc = 0; col = 0;
+            String line = read.nextLine();
+
+            while(loc < line.length())
+            {
+                c = line.charAt(loc++);
+                if(c != ',' && loc != line.length())
+                {
+                    field = field + c;
+                }
+                else
+                {
+                    data[row][col++] = field;
+                    field = "";
+                }
+            }
+            row++;
+        }
+        return data;
     }
 }//end FirstExample
 
